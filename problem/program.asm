ff3e045a61483aedc8feac977bcb994156107b4d
Justjuliko <julioechavarria2002@gmail.com>
2022-02-12 08:59:48 -0500

    Seleccion de tecla

--- problem/program.asm
+++ problem/program.asm
@@ -1,6 +1,18 @@
 // Julio Cesar Echavarria Norena
 // 000419350
 // julio.echavarria@upb.edu.co
-(START)
-    @START
+
+    @24576
+    D=M
+    @70
+    D=D-A
+    @14
+    D;JEQ
+    @24576
+    D=M
+    @67
+    D=D-A
+    @150
+    D;JEQ
+    @0
     0;JMP
